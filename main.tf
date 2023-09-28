5315beb5cbbd1536b21e87d7739b9df14324e6a2
jbonnier <j.bonnier@gmail.com>
2023-09-27 21:34:16 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,129 @@
+locals {
+  enabled              = module.this.enabled
+  logs_kms_key_enabled = length(var.logs_kms_key_arn) == 0
+  logs_kms_key_arn     = local.logs_kms_key_enabled ? module.kms_key.key_arn : data.aws_kms_key.default.0.arn
+  alb_use_existing     = length(var.alb_arn) > 0
+  alb_enabled          = !local.alb_use_existing && var.alb_enabled
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+resource "aws_cloudwatch_log_group" "ecs_insights" {
+  count = local.enabled ? 1 : 0
+
+  name              = "/aws/ecs/containerinsights/${module.this.id}/performance"
+  retention_in_days = var.cloudwatch_log_group_retention_in_days
+  kms_key_id        = local.logs_kms_key_arn
+  tags              = module.this.tags
+}
+
+resource "aws_ecs_cluster" "default" {
+  count = local.enabled ? 1 : 0
+
+  name = module.this.id
+
+  setting {
+    name  = "containerInsights"
+    value = var.ecs_cluster_container_insights_enabled ? "enabled" : "disabled"
+  }
+
+  tags = module.this.tags
+}
+
+data "aws_kms_key" "default" {
+  count = local.logs_kms_key_enabled && local.enabled ? 0 : 1
+
+  key_id = var.logs_kms_key_arn
+}
+
+module "kms_key" {
+  source  = "cloudposse/kms-key/aws"
+  version = "0.12.1"
+
+  enabled = local.logs_kms_key_enabled && local.enabled
+
+  description             = "KMS ECS Cloudwatch Log Groups"
+  deletion_window_in_days = 30
+  enable_key_rotation     = true
+  alias                   = join("", ["alias/", module.this.id])
+  policy                  = data.aws_iam_policy_document.kms_key.json
+
+  context = module.this.context
+}
+
+data "aws_iam_policy_document" "kms_key" {
+  statement {
+    sid       = "Enable IAM User Permissions"
+    actions   = ["kms:*"]
+    effect    = "Allow"
+    resources = ["*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+
+  statement {
+    sid    = "Allow use with Cloudwatch log groups"
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    resources = ["*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
+    }
+
+    condition {
+      test     = "ArnLike"
+      variable = "kms:EncryptionContext:aws:logs:arn"
+      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"]
+    }
+  }
+}
+
+module "lb" {
+  source  = "git@ssh.dev.azure.com:v3/nventive/Terraform-Modules/aws-lb?ref=v0.1.0"
+  enabled = local.alb_enabled && local.enabled
+
+  subnet_ids                = var.subnet_ids
+  ip_address_type           = var.alb_ip_address_type
+  load_balancer_type        = "application"
+  enable_http2              = true
+  security_group_enabled    = true
+  access_logs_enabled       = var.alb_access_logs_enabled
+  access_logs_force_destroy = var.alb_access_logs_force_destroy
+  access_logs_prefix        = var.alb_access_logs_prefix
+
+  context = module.this.context
+}
+
+data "aws_lb" "default" {
+  count = local.alb_use_existing && local.enabled ? 1 : 0
+  arn   = var.alb_arn
+}
+
+module "alb_dns_alias" {
+  source  = "cloudposse/route53-alias/aws"
+  version = "0.13.0"
+
+  enabled   = length(var.alb_dns_aliases) != 0 && local.enabled
+  providers = { aws = aws.route53 }
+
+  aliases          = var.alb_dns_aliases
+  parent_zone_id   = var.parent_zone_id
+  parent_zone_name = var.parent_zone_name
+  target_dns_name  = local.alb_use_existing ? data.aws_lb.default.0.dns_name : module.lb.dns_name
+  target_zone_id   = local.alb_use_existing ? data.aws_lb.default.0.zone_id : module.lb.zone_id
+
+  context = module.this.context
+}
---
+++ variables.tf
@@ -0,0 +1,86 @@
+variable "cloudwatch_log_group_retention_in_days" {
+  type        = number
+  default     = 60
+  description = "Number of days to retain Cloudwatch logs."
+}
+
+variable "ecs_cluster_container_insights_enabled" {
+  type        = bool
+  default     = true
+  description = "Whether or not Container Insights should be enabled."
+}
+
+variable "vpc_id" {
+  type        = string
+  default     = null
+  description = "ID of the VPC. Required if `alb_enabled` is `true`."
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  default     = []
+  description = "List of subnet IDs for the Load Balancer. The Load Balancer will be created in the VPC associated with the subnet IDs."
+}
+
+variable "alb_enabled" {
+  type        = bool
+  default     = true
+  description = <<-EOT
+    Set to false to prevent the module from creating an Application Load Balancer.
+    This setting defaults to false if `alb_arn` is specified.
+  EOT
+}
+
+variable "alb_arn" {
+  type        = string
+  default     = ""
+  description = "Set if you want to use an existing Application Load Balancer."
+}
+
+variable "alb_ip_address_type" {
+  type        = string
+  default     = null
+  description = "Address type for ALB possible. Specify one of `ipv4`, `dualstack`. Only when `alb_enabled = true`."
+}
+
+variable "alb_access_logs_prefix" {
+  type        = string
+  default     = ""
+  description = "Prefix for ALB access logs."
+}
+
+variable "alb_access_logs_enabled" {
+  type        = bool
+  default     = true
+  description = "Whether or not ALB access logs should be enabled."
+}
+
+variable "alb_access_logs_force_destroy" {
+  type        = bool
+  default     = true
+  description = "Whether or not force destroy option should be enabled for the ALB access logs Bucket."
+}
+
+variable "alb_dns_aliases" {
+  type        = list(string)
+  default     = []
+  description = "List of custom domain name aliases for ALB."
+}
+
+variable "parent_zone_id" {
+  type        = string
+  default     = ""
+  description = "ID of the hosted zone to contain this record (or specify `parent_zone_name`). Requires `dns_alias_enabled` set to true."
+}
+
+variable "parent_zone_name" {
+  type        = string
+  default     = ""
+  description = "Name of the hosted zone to contain this record (or specify `parent_zone_id`). Requires `dns_alias_enabled` set to true."
+}
+
+variable "logs_kms_key_arn" {
+  type        = string
+  default     = ""
+  description = "ARN of the KMS key for CloudWatch encryption, if blank, one will be created."
+}
